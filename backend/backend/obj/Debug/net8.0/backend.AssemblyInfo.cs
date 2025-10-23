b81d6350a5609a7ef872eaa53406326bae006521
Chandula Kavishka Wijesekera <chandulakavishka0@gmail.com>
2025-10-23 21:48:31 +0530

    add docker file

--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+760877f1de83f1f3e9173431dcf6e7bf6b66b4c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82f005b38bad395a1fb89dd469176925f95209ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
